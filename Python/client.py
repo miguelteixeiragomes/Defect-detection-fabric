113bb085fd98b97b16651731f837d6c230cee858
Gilberto Loureiro <gilbertoloureiro13894@gmail.com>
2016-05-30 10:12:17 +0100

    Added callback handling. Needs to be tested on Raspberry

--- Python/client.py
+++ Python/client.py
@@ -1,34 +1,65 @@
 from socketIO_client import SocketIO
 import imageController as imc
+import sys
 
+# This function handles the permission requests and responses
+def askForPermission():
+    sio.emit("permission_request")
+    sio.on("permission", on_permission_response)
+    sio.wait()
+    # Commented for future testing. Try this and the above. See what works
+    #sio.emit("permission_request", on_permission_response)
+    #sio.wait_for_callbacks()
+
+# This function handles the process abortion
+def abortProcess():
+    sio.close()
+    imc.killProcess()
+    sys.exit("Process aborted")
+
+# This function is the callback for an image analysis request
+# In case there is NO DEFFECT, asks for permission to send another image
+# In case there IS A DEFFECT, aborts the process
+def imageAnalysisCallback(result):
+    print "Este foi o resultado da analise: " + result
+    if (result == False):
+        askForPermission()
+    else:
+        abortProcess()
+        
+# This function handles the ID request response
 def on_registerID(personalID):
     print "My personal register ID is: " + str(personalID)
     registerID = personalID
 
-def on_permission(data):
+# This function handles the permission granted response
+# Prepares the image data and sends it to the server
+# Waits for response callback
+def on_permission_response(data):
     print "Server Permission: " + str(data)
     # Take picture send base64 to Server
     fileName = imc.capturePicture()
     imageBase64 = imc.convertToBase64(fileName)
-    sio.emit("image", imageBase64)
     # Destroy picture
     imc.remove(fileName)
+    # Send to server
+    sio.emit("image", imageBase64, imageAnalysisCallback)
+    sio.wait_for_callbacks()
 
-def on_kill():
-    imc.killProcess()
-    return True
-
-# Establish the connection & initialize
-# IP needs to be changed to correct server's IP
-sio = SocketIO('192.168.3.1', 5000)
-
-# Request for personal ID
-sio.emit("id_request")
-sio.on("register_id", on_registerID)
-
-# Ask for permission to send picture and wait for the grant
-sio.emit("permission_request")
-sio.on("permission_granted",on_permission)
-sio.on("kill_machine", on_kill)
-
-sio.wait()
+if __name__ == "__main__":
+    try:
+        # Establish the connection & initialize
+        # IP is set to Gil's mac
+        sio = SocketIO('192.168.3.1', 5000)
+        # Request for personal ID
+        sio.emit("id_request")
+        sio.on("register_id", on_registerID)          
+        # Wait for response
+        sio.wait()
+        # Ask for permission
+        askForPermission()
+    except KeyboardInterrupt:
+        # User interrupted, abort process
+        print "Abort by user interrupt"
+        abortProcess()
+    
