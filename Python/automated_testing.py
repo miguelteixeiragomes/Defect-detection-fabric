6c6d0e70393f4b0b33077e2662a31e315126db90
Miguel Gomes <miguel.teixeira.gomes@gmail.com>
2016-05-30 23:30:20 +0100

    agora esta o miguel no comando

--- Python/automated_testing.py
+++ Python/automated_testing.py
@@ -12,7 +12,7 @@ def updateParamList(parameterList, img, defect):
     i = 0
     L = len(parameterList)
     while i < len(parameterList):
-        print '\t', round((len(parameterList) - i) *100. / float(L), 1), '%'
+        print '\t', round((L - len(parameterList) + i) *100. / float(L), 1), '%'
         if fullAnalysis(img, parameterList[i][0], parameterList[i][1], parameterList[i][2]) != defect:
             del(parameterList[i])
         else:
@@ -48,8 +48,6 @@ def testing_siml(images_dir_com, images_dir_sem, fG, sG, threshold):
 
 
 def testing(images_dir_com, images_dir_sem, fG, sG, threshold):    
-    t = localtime()
-    
     fileName_params = 'simls\\ParamsList_' + str(fG.min()) + '_' + str(fG.max()) + '_' + str(fG[1] - fG[0]) + '_' + \
                                              str(sG.min()) + '_' + str(sG.max()) + '_' + str(sG[1] - sG[0]) + '_' + \
                                              str(threshold.min()) + '_' + str(threshold.max()) + '_' + str(threshold[1] - threshold[0]) + '_' + '.p'
@@ -62,15 +60,15 @@ def testing(images_dir_com, images_dir_sem, fG, sG, threshold):
 
     fig = pl.figure(2)
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(np.array([i[0] for i in paramList]), np.array([i[1] for i in paramList]), np.array([i[2] for i in paramList]), c='g', alpha = 0.5)
+    ax.scatter(np.array([i[0] for i in paramList]), np.array([i[1] for i in paramList]), np.array([i[2] for i in paramList]), c = 'g', alpha = 0.5)
     ax.set_xlabel('1st Gauss Blur')
     ax.set_ylabel('2nd Gauss Blur')
     ax.set_zlabel('Threshold')
     pl.show()
     
-fG = np.arange(1, 20, 3)
-sG = np.arange(0.0, 20.0, 5.0)
-threshold = np.arange(1.1, 5.1, 0.5)
+fG = np.arange(1, 25, 1)
+sG = np.arange(0.0, 20.0, 1.0)
+threshold = np.arange(1.1, 5.1, 0.1)
 directory_defect = 'com_defeito\\'
 directory_non_defect = 'sem_defeito\\'
 
