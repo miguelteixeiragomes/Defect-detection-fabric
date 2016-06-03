0e92953d426a4f968770744c0460de1a6cc6268d
Miguel Gomes <miguel.teixeira.gomes@gmail.com>
2016-06-03 19:20:46 +0100

    faster running by keeping all images in ram

--- Python/automated_testing_v2.py
+++ Python/automated_testing_v2.py
@@ -13,18 +13,22 @@ images_dir_com = 'com' + breaker
 images_dir_sem = 'sem' + breaker
 images_com = [images_dir_com + f for f in listdir(images_dir_com) if isfile(join(images_dir_com, f)) and ('.png' in f  or  '.jpg' in f)]
 images_sem = [images_dir_sem + f for f in listdir(images_dir_sem) if isfile(join(images_dir_sem, f)) and ('.png' in f  or  '.jpg' in f)]
+images_com = [np.uint8(np.around(np.average(np.float32(imread(paths)), axis = 2))) for paths in images_com]
+images_sem = [np.uint8(np.around(np.average(np.float32(imread(paths)), axis = 2))) for paths in images_sem]
 
 
 def Q_function(x):
     s = 0
     for i in range(len(images_com)):
         #print '\t', i, '/', len(images_com)
-        img = np.average(np.float32(imread(images_com[i])), axis = 2)
+        #img = np.average(np.float32(imread(images_com[i])), axis = 2)
+        img = np.float32(images_com[i])
         if fullAnalysis(img, x[0], x[1], x[2]) == True:
             s += 1
 
     for i in range(len(images_sem)):
-        img = np.average(np.float32(imread(images_sem[i])), axis = 2)
+        #img = np.average(np.float32(imread(images_sem[i])), axis = 2)
+        img = np.float32(images_sem[i])
         if fullAnalysis(img, x[0], x[1], x[2]) == True:
             s -= 1
 
@@ -32,10 +36,13 @@ def Q_function(x):
 
 
 if __name__ == '__main__':
+    from time import clock
     test = ['function', 'parameterFinder'][0]
 
     if test == 'function':
+        Ti = clock()
         print Q_function([20. , 20.  ,  1.5]) # options: [20.,20.,1.5] ->   ou  20. , 10. , 2.0
+        print 'exec time:', clock() - Ti
 
     if test == 'parameterFinder':
         print conjGradMax(Q_function, [20. , 10. , 2.0])
